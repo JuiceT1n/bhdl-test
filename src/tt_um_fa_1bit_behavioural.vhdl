fea592619a791a129c66fdf2ef652e0e71628358
Justin Sooknanan <114186684+JuiceT1n@users.noreply.github.com>
2026-03-14 13:59:40 -0400

    Update tt_um_fa_1bit_behavioural.vhdl

--- src/tt_um_fa_1bit_behavioural.vhdl
+++ src/tt_um_fa_1bit_behavioural.vhdl
@@ -18,7 +18,7 @@ signal tmp: std_logic_vector(1 downto 0);
 begin  
    process(X1,X2,Cin)
    begin 
- 	tmp <= ('0' & X1) + ('0' & X2) + ('0' & Cin) ;
+ 	tmp <= std_logic_vector(to_unsigned('0' & X1) + to_unsigned('0' & X2) + to_unsigned('0' & Cin)) ;
    end process;
 
    S <= tmp(0);
