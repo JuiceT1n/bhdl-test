d37c60a0c7706801640987ac927860b857c901c9
Justin Sooknanan <114186684+JuiceT1n@users.noreply.github.com>
2026-03-14 14:52:17 -0400

    Update tt_um_fa_1bit_behavioural.vhdl

--- src/tt_um_fa_1bit_behavioural.vhdl
+++ src/tt_um_fa_1bit_behavioural.vhdl
@@ -1,63 +1,32 @@
-
 -- Code adapted from: https://www.fpga4student.com/2017/02/vhdl-code-for-full-adder.html
--- Behavioural code for full adder 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+-- Structural code for full adder 
+library ieee; 
+use ieee.std_logic_1164.all;  
 
-entity tt_um_fa_1bit_behavioural is  
-port( 
+entity fa_1bit_structural is  
+port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
+        uio_in  : in  std_logic_vector(7 downto 0);
+        uio_out : out std_logic_vector(7 downto 0);
+        uio_oe  : out std_logic_vector(7 downto 0);
         ena     : in  std_logic;
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
-  
-end entity tt_um_fa_1bit_behavioural;
-architecture Behavioral of tt_um_fa_1bit_behavioural is   
- 
-signal tmp: std_logic_vector(2 downto 0);
-signal ur_S, ur_Cout : std_logic;
- 
+
+end entity fa_1bit_structural;  
+
+architecture structural of fa_1bit_structural is  
+
+   signal a1, a2, a3: std_logic;  
+
 begin  
 
-tmp <= ui_in(0) & ui_in(1) & ui_in(2);
-  
-ur_S <= '0' when tmp="000" else
-    '1' when tmp="001" else
-    '0' when tmp="010" else
-    '1' when tmp="011" else
-    '0' when tmp="100" else
-    '1' when tmp="101" else
-    '0' when tmp="110" else
-     '1';
+   a1 <= ui_in(0) xor ui_in(1);  
+   a2 <= ui_in(1) and ui_in(2);  
+   a3 <= ui_in(0) and ui_in(2);  
+   uo_out(0) <= ui_in(1) or ui_in(2);  
+   uo_out(1) <= a1 xor ui_in(0);  
 
-ur_Cout <= '0' when tmp="000" else
-    '1' when tmp="001" else
-    '0' when tmp="010" else
-    '1' when tmp="011" else
-    '0' when tmp="100" else
-    '1' when tmp="101" else
-    '0' when tmp="110" else
-     '1';
-
-  process(clk, ena, rst_n)
-
-  begin
-
-    if (rising_edge(clk)) then
-      if (rst_n = '1') then
-         uo_out(0) <= '0';
-        uo_out(1) <= '0';
-      else
-        if (ena = '1') then
-          uo_out(0) <= ur_S;
-          uo_out(1) <= ur_Cout;
-        end if;
-      end if;
-    end if;
-
-  end process;
-
-end Behavioral; 
+end structural;  
